eaa668210084d7bf74ad7579f3f4f902205d188e
makacies <barberrywork@gmail.com>
2022-05-30 20:56:00 +0300

    whisky classification

--- whiskey_classification/classification.py
+++ whiskey_classification/classification.py
@@ -26,7 +26,28 @@ plt.axis("tight")
 plt.colorbar()
 # plt.savefig("corr_whisky.pdf")
 
-from sklearn.cluster.bicluster import SpectralCoclustering
+# check from here =>
+from sklearn.cluster import SpectralCoclustering
+model = SpectralCoclustering(n_clusters=6, random_state=0)
+model.fit(corr_whisky)
+print(model)
 
+print(np.sum(model.rows_. axis=1))
+print(np.sum(model.rows_. axis=0))
+print(model.row_labels_)
 
-# print(corr_flavors)
+whisky['Group'] = pd.Series(model.row_labels_, index=whisky.index)
+whisky = whisky.iloc[np.argsort(model.row_labels_)]
+whisky = whisky.reset_index(drop=True)
+correlation = pd.DataFrame.corr(whisky.iloc[:,2:14].transpose())
+
+plt.figure(figsize = (14,7))
+plt.subplot(121)
+plt.pcolor(corr_whisky)
+plt.title("Original")
+plt.axis("tight")
+plt.subplot(122)
+plt.pcolor(correlation)
+plt.title("Rearranged")
+plt.axis("tight")
+plt.savefig("correlation.pdf")
